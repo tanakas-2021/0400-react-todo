1ba41f5ab37916dddc17f8cce00d38d005c58f55
tanakas-2021 <shugotanaka2020@gmail.com>
2024-12-25 15:42:30 +0900

    wip

--- app/page.js
+++ app/page.js
@@ -1,95 +1,111 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { useState } from "react";
+import styles from "./page.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [taskName, setTaskName] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [isDisplayDone, setIsDisplayDone] = useState(false);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const [tasks, setTasks] = useState([
+    { taskName: "task1", dueDate: "2024-01-25", isDone: false },
+    { taskName: "task2", dueDate: "2024-02-05", isDone: false },
+  ]);
+
+  const handleIsDisplayDone = () => {
+    const newIsDisplayDone = !isDisplayDone;
+    setIsDisplayDone(newIsDisplayDone);
+  };
+
+  const handleCheckboxClick = (index) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task, i) =>
+        i === index ? { ...task, isDone: !task.isDone } : task
+      )
+    );
+  };
+
+  const addOnClick = () => {
+    const newTasks = [
+      ...tasks,
+      { taskName: taskName, dueDate: dueDate, isDone: false },
+    ];
+    setTasks(newTasks);
+  };
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Todo</h1>
+      </header>
+      <main className={styles.main}>
+        <form className={styles.form}>
+          <div className={styles.formGroups}>
+            <div className={styles.inputTask}>
+              <label className={styles.inputLabel}>タスク</label>
+              <input
+                className={styles.input}
+                placeholder="タスク名を入力"
+                type="text"
+                onChange={(e) => setTaskName(e.target.value)}
+                value={taskName}
+              />
+            </div>
+            <div className={styles.inputDate}>
+              <label
+                className={styles.inputLabel}
+                onChange={(e) => setDueDate(e.target.value)}
+                value={dueDate}
+              >
+                期限日
+              </label>
+              <input className={styles.input} type="date" />
+            </div>
+          </div>
+          <div className={styles.formFooter}>
+            <button className={styles.button}>追加</button>
+          </div>
+        </form>
+        <div>
+          <div className={styles.tableSetting}>
+            <label>
+              <input type="checkbox" onClick={handleIsDisplayDone} />
+              完了タスクを表示
+            </label>
+          </div>
+          <div className={styles.tableHeader}>
+            <div className={styles.tableHeaderCell}></div>
+            <div className={styles.tableHeaderCellTask}>タスク</div>
+            <div className={styles.tableHeaderCell}>期限日</div>
+            <div className={styles.tableHeaderCell}></div>
+          </div>
+          <div>
+            {tasks.map((task, index) => {
+              return (
+                <div className={styles.tableRow} key={index}>
+                  <div className={styles.tableCellCenter}>
+                    <div
+                      className={
+                        task.isDone ? styles.checkboxChecked : styles.checkbox
+                      }
+                    >
+                      <div onClick={() => handleCheckboxClick(index)}>
+                        <FontAwesomeIcon icon={faCheck} size="2x" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.tableCellTask}>{task.taskName}</div>
+                  <div className={styles.tableCell}>{task.dueDate}</div>
+                  <div className={styles.tableCell}></div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
